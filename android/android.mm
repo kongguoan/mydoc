3fbf05460f133848509359fa868152e0c981f345
kongguoan <158520812@qq.com>
2013-06-27 13:39:21 +0800

    1
    1

---
+++ android/android.mm
@@ -0,0 +1,141 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1372302941362" ID="ID_1968208090" MODIFIED="1372305756289" TEXT="">
+<font NAME="SansSerif" SIZE="20"/>
+<node COLOR="#0033ff" CREATED="1372303474979" ID="ID_172985541" MODIFIED="1372306754256" POSITION="right" TEXT="Activity(&#x6d3b;&#x52a8;)">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1372306731234" ID="ID_389581377" MODIFIED="1372306739220" TEXT="active&#x662f;&#x4ec0;&#x4e48;">
+<node CREATED="1372306757288" ID="ID_721403860" MODIFIED="1372306757288">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-right: 0px; letter-spacing: normal; word-spacing: 0px; font-variant: normal; font-size: 14px; margin-top: 0px; white-space: normal; color: rgb(34, 34, 34); line-height: 19px; text-transform: none; font-style: normal; margin-bottom: 15px; text-align: start; background-color: rgb(249, 249, 249); font-family: Roboto, sans-serif; font-weight: normal; text-indent: 0px; margin-left: 0px">
+      &#19968;&#20010;<a style="color: rgb(37, 138, 175); text-decoration: none" href="http://developer.android.com/reference/android/app/Activity.html"><code style="font-variant: normal; font-size: 13px; color: rgb(0, 102, 0); line-height: 14px; font-style: normal; font-family: monospace; font-weight: normal"><font color="rgb(37, 138, 175)" face="monospace" size="13px">&#27963;&#21160;</font></code></a>&#26159;&#19968;&#20010;&#24212;&#29992;&#31243;&#24207;&#32452;&#20214;&#65292;&#23427;&#25552;&#20379;&#20102;&#19968;&#20010;&#23631;&#24149;&#65292;&#29992;&#25143;&#21487;&#20197;&#20114;&#21160;&#65292;&#20570;&#19968;&#20123;&#20107;&#24773;&#65292;&#22914;&#25320;&#25171;&#30005;&#35805;&#65292;&#25293;&#29031;&#65292;&#21457;&#36865;&#30005;&#23376;&#37038;&#20214;&#65292;&#25110;&#26597;&#30475;&#22320;&#22270;&#12290;&#27599;&#19968;&#20010;&#27963;&#21160;&#30340;&#19968;&#20010;&#31383;&#21475;&#65292;&#22312;&#20854;&#20013;&#32472;&#21046;&#23427;&#30340;&#29992;&#25143;&#30028;&#38754;&#12290;&#31383;&#21475;&#36890;&#24120;&#20805;&#28385;&#23631;&#24149;&#65292;&#20294;&#21487;&#33021;&#23567;&#20110;&#20854;&#20182;&#31383;&#21475;&#19978;&#30340;&#23631;&#24149;&#21644;&#28014;&#21160;&#12290;
+    </p>
+    <p style="margin-right: 0px; letter-spacing: normal; word-spacing: 0px; font-variant: normal; font-size: 14px; margin-top: 0px; white-space: normal; color: rgb(34, 34, 34); line-height: 19px; text-transform: none; font-style: normal; margin-bottom: 15px; text-align: start; background-color: rgb(249, 249, 249); font-family: Roboto, sans-serif; font-weight: normal; text-indent: 0px; margin-left: 0px">
+      &#24212;&#29992;&#31243;&#24207;&#36890;&#24120;&#26159;&#30001;&#24444;&#27492;&#26494;&#25955;&#22320;&#32465;&#23450;&#20110;&#22810;&#20010;&#27963;&#21160;&#12290;&#36890;&#24120;&#24773;&#20917;&#19979;&#65292;&#34987;&#25351;&#23450;&#20026;&#8220;&#20027;&#8221;&#30340;&#27963;&#21160;&#65292;&#36825;&#26159;&#22312;&#31532;&#19968;&#27425;&#21551;&#21160;&#24212;&#29992;&#31243;&#24207;&#26102;&#65292;&#21576;&#29616;&#32473;&#29992;&#25143;&#22312;&#24212;&#29992;&#31243;&#24207;&#20013;&#30340;&#19968;&#20010;&#27963;&#21160;&#12290;&#27599;&#19968;&#20010;&#27963;&#21160;&#23601;&#21487;&#20197;&#24320;&#22987;&#21478;&#19968;&#20010;&#27963;&#21160;&#65292;&#20197;&#25191;&#34892;&#19981;&#21516;&#30340;&#21160;&#20316;&#12290;&#27599;&#27425;&#24320;&#22987;&#19968;&#20010;&#26032;&#30340;&#27963;&#21160;&#65292;&#20572;&#27490;&#20197;&#21069;&#30340;&#27963;&#21160;&#65292;&#20294;&#26159;&#65292;&#31995;&#32479;&#20250;&#20445;&#30041;&#19968;&#20010;&#22534;&#26632;&#20013;&#30340;&#27963;&#21160;&#65288;&#8220;&#21518;&#36864;&#22534;&#26632;&#8221;&#65289;&#12290;&#24403;&#24320;&#22987;&#19968;&#20010;&#26032;&#30340;&#27963;&#21160;&#65292;&#23427;&#26159;&#25512;&#21040;&#21518;&#36864;&#22534;&#26632;&#21644;&#29992;&#25143;&#20851;&#27880;&#30340;&#28966;&#28857;&#12290;&#35813;&#30340;&#21518;&#36864;&#22534;&#26632;&#36981;&#23432;&#22522;&#26412;&#8220;&#26368;&#21518;&#65292;&#20808;&#20986;&#8221;&#22534;&#26632;&#26426;&#21046;&#65292;&#22240;&#27492;&#65292;&#24403;&#29992;&#25143;&#23436;&#25104;&#24403;&#21069;&#27963;&#21160;&#65292;&#24182;&#25353;&#19979;&#8220;&#160;<i><em style="font-style: italic">&#36820;&#22238;&#8220;</em></i>&#25353;&#38062;&#65292;&#23427;&#24377;&#20986;&#22534;&#26632;&#65288;&#21644;&#38144;&#27585;&#65289;&#21644;&#20197;&#21069;&#30340;&#27963;&#21160;&#24674;&#22797;&#12290;&#65288;&#21518;&#36864;&#22534;&#26632;<a style="color: rgb(37, 138, 175); text-decoration: none" href="http://developer.android.com/guide/components/tasks-and-back-stack.html"><font color="rgb(37, 138, 175)">&#20219;&#21153;&#21644;&#36820;&#22238;&#22534;&#26632;</font></a>&#25991;&#20214;&#20013;&#35814;&#32454;&#35752;&#35770;&#12290;&#65289;
+    </p>
+    <p style="margin-right: 0px; letter-spacing: normal; word-spacing: 0px; font-variant: normal; font-size: 14px; margin-top: 0px; white-space: normal; color: rgb(34, 34, 34); line-height: 19px; text-transform: none; font-style: normal; margin-bottom: 15px; text-align: start; background-color: rgb(249, 249, 249); font-family: Roboto, sans-serif; font-weight: normal; text-indent: 0px; margin-left: 0px">
+      &#24403;&#27963;&#21160;&#24320;&#22987;&#19968;&#20010;&#26032;&#30340;&#27963;&#21160;&#20572;&#27490;&#65292;&#22240;&#20026;&#65292;&#23427;&#26159;&#36890;&#36807;&#27963;&#21160;&#30340;&#29983;&#21629;&#21608;&#26399;&#22238;&#35843;&#26041;&#27861;&#65292;&#36825;&#31181;&#29366;&#24577;&#25913;&#21464;&#36890;&#30693;&#12290;&#26377;&#20960;&#20010;&#22238;&#35843;&#26041;&#27861;&#65292;&#21487;&#33021;&#20250;&#25910;&#21040;&#19968;&#20010;&#27963;&#21160;&#65292;&#30001;&#20110;&#19968;&#25913;&#20854;&#29366;&#24577;&#30340;&#31995;&#32479;&#26159;&#21542;&#21019;&#24314;&#65292;&#20572;&#27490;&#65292;&#24674;&#22797;&#65292;&#25110;&#25703;&#27585;&#23427;&#65292;&#21644;&#27599;&#20010;&#22238;&#35843;&#20026;&#24744;&#25552;&#20379;&#25191;&#34892;&#29305;&#23450;&#30340;&#24037;&#20316;&#65292;&#36866;&#24403;&#30340;&#26426;&#20250;&#35813;&#29366;&#24577;&#26356;&#25913;&#12290;&#20363;&#22914;&#65292;&#20572;&#27490;&#30340;&#26102;&#20505;&#65292;&#20320;&#30340;&#27963;&#21160;&#24212;&#35813;&#37322;&#25918;&#20219;&#20309;&#22823;&#30340;&#23545;&#35937;&#65292;&#22914;&#32593;&#32476;&#25110;&#25968;&#25454;&#24211;&#36830;&#25509;&#12290;&#24403;&#27963;&#21160;&#30340;&#24674;&#22797;&#65292;&#24744;&#21487;&#20197;&#37325;&#26032;&#33719;&#21462;&#24517;&#35201;&#30340;&#36164;&#28304;&#21644;&#24674;&#22797;&#34987;&#25171;&#26029;&#30340;&#34892;&#21160;&#12290;&#36825;&#20123;&#29366;&#24577;&#36716;&#25442;&#26159;&#27963;&#21160;&#30340;&#29983;&#21629;&#21608;&#26399;&#30340;&#25152;&#26377;&#37096;&#20998;&#12290;
+    </p>
+    <p style="margin-right: 0px; letter-spacing: normal; word-spacing: 0px; font-variant: normal; font-size: 14px; margin-top: 0px; white-space: normal; color: rgb(34, 34, 34); line-height: 19px; text-transform: none; font-style: normal; margin-bottom: 15px; text-align: start; background-color: rgb(249, 249, 249); font-family: Roboto, sans-serif; font-weight: normal; text-indent: 0px; margin-left: 0px">
+      &#26412;&#25991;&#26723;&#20854;&#20313;&#37096;&#20998;&#23558;&#35752;&#35770;&#22914;&#20309;&#24314;&#31435;&#21644;&#20351;&#29992;&#27963;&#21160;&#65292;&#21253;&#25324;&#19968;&#20010;&#23436;&#25972;&#30340;&#35752;&#35770;&#27963;&#21160;&#30340;&#29983;&#21629;&#21608;&#26399;&#26159;&#22914;&#20309;&#24037;&#20316;&#30340;&#22522;&#30784;&#65292;&#25152;&#20197;&#20320;&#21487;&#20197;&#27491;&#30830;&#22320;&#31649;&#29702;&#21508;&#31181;&#27963;&#21160;&#29366;&#24577;&#20043;&#38388;&#30340;&#36807;&#28193;&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1372306669330" ID="ID_1187827018" MODIFIED="1372306682917" TEXT="&#x521b;&#x5efa;active">
+<node CREATED="1372306692589" ID="ID_649329655" MODIFIED="1372306692589">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-right: 0; letter-spacing: normal; word-spacing: 0px; font-variant: normal; font-size: small; margin-top: 10px; white-space: normal; color: rgb(0, 0, 0); padding-top: 0px; padding-right: 0px; text-transform: none; margin-bottom: 10px; font-style: normal; text-align: start; background-color: rgb(255, 255, 255); padding-left: 0px; font-family: verdana, arial, helvetica, sans-serif; padding-bottom: 0px; text-indent: 0px; font-weight: normal; margin-left: 0">
+      &#21019;&#24314;&#19968;&#20010;activity&#65292;&#20320;&#24517;&#39035;&#21019;&#24314;&#19968;&#20010;Activity&#30340;&#23376;&#31867;&#65288;&#25110;&#32773;&#19968;&#20010;Activity&#30340;&#23376;&#31867;&#30340;&#23376;&#31867;&#65289;&#12290;&#22312;&#20320;&#30340;&#23376;&#31867;&#20013;&#65292;&#20320;&#38656;&#35201;&#23454;&#29616;&#31995;&#32479;&#22238;&#35843;&#30340;&#22238;&#35843;&#26041;&#27861;&#65292;&#24403;activity&#22312;&#23427;&#30340;&#29983;&#21629;&#21608;&#26399;&#30340;&#22810;&#31181;&#29366;&#24577;&#20013;&#36716;&#25442;&#30340;&#26102;&#20505;&#65292;&#20363;&#22914;&#24403;activity&#34987;&#21019;&#24314;&#12289;&#20572;&#27490;&#12289;&#24674;&#22797;&#25110;&#25703;&#27585;&#12290;&#20004;&#20010;&#26368;&#37325;&#35201;&#30340;&#22238;&#35843;&#26041;&#27861;&#26159;&#65306;
+    </p>
+    <code style="letter-spacing: normal; font-variant: normal; word-spacing: 0px; font-size: medium; color: rgb(0, 0, 102); white-space: normal; line-height: 30px; text-transform: none; font-style: normal; text-align: start; background-color: rgb(255, 255, 255); font-family: verdana, arial, helvetica, sans-serif; font-weight: normal; text-indent: 0px"><font color="rgb(0, 107, 173)" face="verdana, arial, helvetica, sans-serif" size="medium"><a style="color: rgb(0, 107, 173); text-decoration: none" href="file:///C:/Program%20Files/Android/android-sdk/docs/reference/android/app/Activity.html#onCreate(android.os.Bundle)">onCreate()</a></font></code>&#20320;&#24517;&#39035;&#23454;&#29616;&#36825;&#20010;&#26041;&#27861;&#12290;&#31995;&#32479;&#35843;&#29992;&#23427;&#24403;&#21019;&#24314;&#20320;&#30340;activity&#30340;&#26102;&#20505;&#12290;&#22312;&#20320;&#30340;&#23454;&#29616;&#20013;&#65292;&#20320;&#24212;&#35813;&#21021;&#22987;&#21270;&#20320;&#30340;activity&#30340;&#22522;&#26412;&#30340;&#32452;&#20214;&#12290;&#26356;&#37325;&#35201;&#30340;&#26159;&#65292;&#36825;&#37324;&#23601;&#26159;&#20320;&#24517;&#39035;&#35843;&#29992;setContentView&#65288;&#65289;&#26469;&#23450;&#20041;activity&#29992;&#25143;&#25509;&#21475;&#32780;&#24050;&#30340;&#22320;&#26041;&#12290;<code style="letter-spacing: normal; font-variant: normal; word-spacing: 0px; font-size: medium; color: rgb(0, 0, 102); white-space: normal; line-height: 30px; text-transform: none; font-style: normal; text-align: start; background-color: rgb(255, 255, 255); font-family: verdana, arial, helvetica, sans-serif; font-weight: normal; text-indent: 0px"><font color="rgb(0, 107, 173)" face="verdana, arial, helvetica, sans-serif" size="medium"><a style="color: rgb(0, 107, 173); text-decoration: none" href="file:///C:/Program%20Files/Android/android-sdk/docs/reference/android/app/Activity.html#onPause()">onPause()</a></font></code>&#31995;&#32479;&#35843;&#29992;&#36825;&#20010;&#26041;&#27861;&#24403;&#29992;&#25143;&#31163;&#24320;&#20320;&#30340;activity&#65288;&#34429;&#28982;&#19981;&#24635;&#26159;&#24847;&#21619;&#30528;activity&#34987;&#25703;&#27585;&#65289;&#12290;&#36825;&#36890;&#24120;&#26159;&#20320;&#24212;&#35813;&#25552;&#20132;&#20219;&#20309;&#21464;&#21270;&#65292;&#37027;&#27492;&#23558;&#20250;&#36229;&#36234;user session&#32780;&#23384;&#22312;&#30340;&#65288;&#22240;&#20026;&#29992;&#25143;&#21487;&#33021;&#19981;&#20877;&#22238;&#26469;&#65289;&#12290;
+
+    <p style="margin-right: 0; letter-spacing: normal; word-spacing: 0px; font-variant: normal; font-size: small; margin-top: 10px; white-space: normal; color: rgb(0, 0, 0); padding-top: 0px; padding-right: 0px; text-transform: none; margin-bottom: 10px; font-style: normal; text-align: start; background-color: rgb(255, 255, 255); padding-left: 0px; font-family: verdana, arial, helvetica, sans-serif; padding-bottom: 0px; text-indent: 0px; font-weight: normal; margin-left: 0">
+      &#12288;&#12288;&#26377;&#33509;&#24178;&#20854;&#23427;&#29983;&#21629;&#21608;&#26399;&#22238;&#35843;&#20989;&#25968;&#20320;&#24212;&#35813;&#20351;&#29992;&#20026;&#20102;&#25552;&#20379;&#19968;&#20010;&#27969;&#30021;&#30340;&#29992;&#25143;&#20307;&#39564;&#65292;&#24182;&#34920;&#25805;&#20316;&#24322;&#24120;&#20013;&#26029;&#20250;&#24341;&#36215;&#20320;&#30340;activity&#34987;&#20013;&#26029;&#29978;&#33267;&#34987;&#25703;&#27585;&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1372303822385" ID="ID_1482652627" MODIFIED="1372303901990" TEXT="&#x56db;&#x4e2a;&#x57fa;&#x672c;&#x72b6;&#x6001;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1372303843785" ID="ID_1239188516" MODIFIED="1372303985335" TEXT="Active/Runing">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(0, 0, 0)" face="arial, nsimsun, sans-serif" size="15px">&#19968;&#20010;&#26032; Activity &#21551;&#21160;&#20837;&#26632;&#21518;&#65292;&#23427;&#22312;&#23631;&#24149;&#26368;&#21069;&#31471;&#65292;&#22788;&#20110;&#26632;&#30340;&#26368;&#39030;&#31471;&#65292;&#27492;&#26102;&#23427;&#22788;&#20110;&#21487;&#35265;&#24182;&#21487;&#21644;&#29992;&#25143;&#20132;&#20114;&#30340;&#28608;&#27963;&#29366;&#24577;&#12290;</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1372303879273" ID="ID_421693094" MODIFIED="1372303998119" TEXT="Paused ">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(0, 0, 0)" face="arial, nsimsun, sans-serif" size="15px">&#24403; Activity &#34987;&#21478;&#19968;&#20010;&#36879;&#26126;&#25110;&#32773; Dialog &#26679;&#24335;&#30340; Activity &#35206;&#30422;&#26102;&#30340;&#29366;&#24577;&#12290;&#27492;&#26102;&#23427;&#20381;&#28982;&#19982;&#31383;&#21475;&#31649;&#29702;&#22120;&#20445;&#25345;&#36830;&#25509;&#65292;&#31995;&#32479;&#32487;&#32493;&#32500;&#25252;&#20854;&#20869;&#37096;&#29366;&#24577;&#65292;&#25152;&#20197;&#23427;&#20173;&#28982;&#21487;&#35265;&#65292;&#20294;&#23427;&#24050;&#32463;&#22833;&#21435;&#20102;&#28966;&#28857;&#25925;&#19981;&#21487;&#19982;&#29992;&#25143;&#20132;&#20114;&#12290;</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1372303928145" ID="ID_1411943584" MODIFIED="1372304020973" TEXT="Stoped">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(0, 0, 0)" face="arial, nsimsun, sans-serif" size="15px">&#24403; Activity &#34987;&#21478;&#22806;&#19968;&#20010; Activity &#35206;&#30422;&#12289;&#22833;&#21435;&#28966;&#28857;&#24182;&#19981;&#21487;&#35265;&#26102;&#22788;&#20110;</font><font color="rgb(0, 0, 0)" face="arial, nsimsun, sans-serif">&#160;</font><font color="rgb(0, 0, 0)" face="arial, nsimsun, sans-serif" size="15px"><strong style="letter-spacing: normal; word-spacing: 0px; font-variant: normal; font-size: 15px; white-space: normal; color: rgb(0, 0, 0); line-height: normal; text-transform: none; text-align: left; background-color: rgb(255, 255, 255); font-style: normal; font-family: arial, nsimsun, sans-serif; text-indent: 0px"><em>Stop</em></strong><em><strong style="letter-spacing: normal; font-variant: normal; word-spacing: 0px; font-size: 15px; color: rgb(0, 0, 0); white-space: normal; line-height: normal; text-transform: none; font-style: normal; background-color: rgb(255, 255, 255); text-align: left; font-family: arial, nsimsun, sans-serif; text-indent: 0px">ed</strong></em>&#29366;&#24577;&#12290;</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1372303949914" ID="ID_1007877703" MODIFIED="1372304028407" TEXT="Killed ">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(0, 0, 0)" face="arial, nsimsun, sans-serif" size="15px">Activity &#34987;&#31995;&#32479;&#26432;&#27515;&#22238;&#25910;&#25110;&#32773;&#27809;&#26377;&#34987;&#21551;&#21160;&#26102;&#22788;&#20110;&#160;<strong style="letter-spacing: normal; word-spacing: 0px; font-variant: normal; font-size: 15px; white-space: normal; color: rgb(0, 0, 0); line-height: normal; text-transform: none; text-align: left; background-color: rgb(255, 255, 255); font-style: normal; font-family: arial, nsimsun, sans-serif; text-indent: 0px"><em>Killed</em></strong>&#29366;&#24577;&#12290;</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1372304050929" ID="ID_145939031" MODIFIED="1372304502822" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#29983;&#21629;&#21608;&#26399;
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1372304454985" HGAP="44" ID="ID_985196843" MODIFIED="1372304486352" VSHIFT="2">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="android-1.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1372305118689" ID="ID_639333082" MODIFIED="1372305125182" TEXT="&#x72b6;&#x6001;&#x8f6c;&#x6362;"/>
+</node>
+</node>
+</map>
